e73644f26954520ec4096da91fa43a7eeea525cb
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2024-02-20 21:23:36 +0100

    Create GraphicBar composable
    Defines the `GraphicBar` composable function to represent a single bar in a bar chart. Takes in parameters such as modifier, bar data, colors, and text styles to customize the appearance of the bar. Calculates the bar height based on the provided data and animates its height and color changes. Utilizes animated visibility to show or hide the bubble containing the bar's value when the bar is selected. Enhances the interactivity and visual appeal of the bar chart by encapsulating bar rendering and animation logic within a dedicated composable function.

--- simplecomchart/src/main/java/github/returdev/simplecomchart/components/bar/GraphicBar.kt
+++ simplecomchart/src/main/java/github/returdev/simplecomchart/components/bar/GraphicBar.kt
@@ -1,12 +1,26 @@
 package github.returdev.simplecomchart.components.bar
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -19,8 +33,108 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import github.returdev.simplecomchart.components.bar.bubble.SelectedGraphicBarBubble
+import github.returdev.simplecomchart.components.bar.model.GraphicBarData
 import github.returdev.simplecomchart.components.bar.model.GraphicBarFixedMeasures
 import github.returdev.simplecomchart.components.core.extension.noRippleClickable
+import github.returdev.simplecomchart.components.core.graph.GraphicBarChartColors
+
+
+/**
+ * This is a Composable function that represents a GraphicBar in a bar chart.
+ *
+ * @param modifier The modifier to be applied to the GraphicBar. Default is Modifier.
+ * @param contentHeightDpPerUnit The height of the content per unit.
+ * @param graphicBarData The data representing the graphic bar.
+ * @param colors The [GraphicBarChartColors] object that defines the colors for the graphic bar.
+ * @param bubbleTextStyle The [TextStyle] object that defines the text style for the bubble content.
+ * @param barNameTextStyle The [TextStyle] object that defines the text style for the bar name.
+ * @param isSelected A function that returns a boolean indicating whether the bar is selected.
+ * @param onClick The callback to be invoked when the bar is clicked.
+ */
+@Composable
+fun GraphicBar(
+    modifier : Modifier = Modifier,
+    contentHeightDpPerUnit : Dp,
+    graphicBarData : GraphicBarData,
+    colors : GraphicBarChartColors,
+    bubbleTextStyle : TextStyle,
+    barNameTextStyle : TextStyle,
+    isSelected : Boolean,
+    onClick : () -> Unit
+) {
+
+    val measures = GraphicBarFixedMeasures
+
+    // Calculate the height of the bar based on the bar value and the height per unit
+    val barHeight = remember(graphicBarData) {
+        mutableStateOf(
+            measures.calculateBarHeight(
+                contentHeightDpPerUnit,
+                graphicBarData.barValue
+            )
+        )
+    }
+
+    val animatedBarHeight = animateDpAsState(
+        animationSpec = tween(500),
+        targetValue = barHeight.value,
+        label = "bar_height_animation"
+    )
+
+//     Animated color of the bar
+    val barColor = animateColorAsState(
+        targetValue = colors.barColor(selected = isSelected),
+        label = "bar_color_animation"
+    )
+
+//     Animated color of the bar name
+    val barNameColor = animateColorAsState(
+        targetValue = colors.barNameColor(selected = isSelected),
+        label = "bar_name_color_animation"
+    )
+
+    // Create a Column to hold the bar and its bubble
+    Column(
+        modifier = Modifier
+            .requiredWidth(measures.graphicBarRootWidth)
+            .fillMaxHeight()
+            .padding(0.dp)
+            .then(modifier),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+
+        // Create an AnimatedVisibility that shows or hides the bubble based on whether the bar is selected
+        AnimatedVisibility(
+            visible = isSelected,
+            enter = slideInVertically { it } + fadeIn(animationSpec = tween(500)),
+            exit = slideOutVertically(animationSpec = tween(500)) { it } + fadeOut(animationSpec = tween(200))
+        ) {
+
+            // Create a SelectedGraphicBarBubble that shows the value of the bar
+            SelectedGraphicBarBubble(
+                contentText = graphicBarData.barShowValue,
+                bubbleColor = colors.barColor(selected = true),
+                onBubbleColor = colors.onBarColor(selected = true),
+                textStyle = bubbleTextStyle
+            )
+
+        }
+
+        // Create a Bar that represents the bar in the bar chart
+        Bar(
+            barName = graphicBarData.barName,
+            barHeight = animatedBarHeight,
+            barColor = barColor,
+            nameColor = barNameColor,
+            textStyle = barNameTextStyle,
+            onClick = onClick
+        )
+
+    }
+
+}
 
 
 /**
