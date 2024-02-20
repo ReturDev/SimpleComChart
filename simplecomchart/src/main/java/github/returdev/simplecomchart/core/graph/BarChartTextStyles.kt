d882c4ac2ac1fc4a3e8313766b809f9b837d2b6c
DESKTOP-OT04G7O\Sergio <sergiomm50@gmail.com>
2024-02-20 22:02:49 +0100

    Create BarChartTextStyles data class
    - Define an immutable data class representing text styles used in the bar chart.
    - Encapsulates text styles for various components of the bar chart, including bubbles, bar names, units, goal indicators, and value indicators.

---
+++ simplecomchart/src/main/java/github/returdev/simplecomchart/core/graph/BarChartTextStyles.kt
@@ -0,0 +1,23 @@
+package github.returdev.simplecomchart.core.graph
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.TextStyle
+
+
+/**
+ * Immutable data class representing text styles used in a graphic bar chart.
+ *
+ * @property barChartBubbleStyle The text style for the bubble in the bar chart.
+ * @property barChartNameStyle The text style for the names of bars in the bar chart.
+ * @property graphicUnitStyle The text style for the unit displayed in the bar chart.
+ * @property goalIndicatorStyle The text style for the goal indicator in the bar chart.
+ * @property graphicValueIndicatorStyle The text style for the value indicator in the bar chart.
+ */
+@Immutable
+data class BarChartTextStyles internal constructor(
+    internal val barChartBubbleStyle : TextStyle,
+    internal val barChartNameStyle : TextStyle,
+    internal val graphicUnitStyle : TextStyle,
+    internal val goalIndicatorStyle : TextStyle,
+    internal val graphicValueIndicatorStyle : TextStyle
+)
